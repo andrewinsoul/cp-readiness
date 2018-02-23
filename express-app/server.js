e7d2ca5d02e8cd6531b4ac6cccdf8b3a545de176
Andrew Okoye <andrewinsoul@gmail.com>
2018-02-23 15:35:47 -0800

    modify the server file

--- express-app/server.js
+++ express-app/server.js
@@ -4,17 +4,19 @@ const router = express.Router();
 const path = __dirname + '/views';
 const filePath = `${__dirname}/views`;
 
+app.use('/static', express.static('public'));
 router.use((req, res, next) => {
     console.log("/" + req.method);
     next();
 });
 
+// app.use('/static', express.static('public'));
 router.get("/", (req, res) => res.sendFile(filePath+'/index.html'));
 router.get("/about", (req, res) => res.sendFile(filePath+'/about.html'));
 router.get("/contact", (req, res) => res.sendFile(filePath+'/contact.html'));
 
+
 app.use("/", router);
-app.use("*", (req, res) => res.sendFile(path + "/404.html"));
-app.use('/static', express.static('public'));
+app.use("*", (req, res) => res.sendFile(path + "/404.html"))
 
 app.listen(3000, () => console.log("Live at Port 3000"));
